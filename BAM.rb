32a4c64aa6ae897b5fac6a447dcd508bff92fc9a
rob <rob@rob-laptop.(none)>
2010-12-25 20:27:20 +1300

    putting args and constants in main file

--- BAM.rb
+++ BAM.rb
@@ -1,15 +1,27 @@
-PATH = File.expand_path(File.dirname(__FILE__))
 require 'rubygems'
 require 'builder'
 
+PATH = File.expand_path(File.dirname(__FILE__))
+
+SCRIPT = ARGV[0]
+
+SCRIPT_PATH = PATH + '/' + SCRIPT
+
+PUBLIC = 'resources/public'
+
+HOST = '0.0.0.0'
+
+PORT = 5000
+
 $:.unshift PATH
+
+
 
 require 'helpers/colourize.rb'
 require 'helpers/net_tools.rb'
-require 'resources/views.rb'
+require 'runner.rb'
+require 'resources/view.rb'
 require 'server/server.rb'
-
-SCRIPT = ARGV[0]
 
 $OPTS = Hash.new
 $OPTS[:host] = "0.0.0.0"
@@ -17,9 +29,7 @@ $OPTS[:port] = 5000
 $OPTS[:root] = ""
 $OPTS[:script] = 'hello.rb'
 
-
-Views.build
-
+View.new_session
 server = Server
 server.start
 
